c8e8625285f60d836cf037849b66a26586239ae9
Pedro Célio <pedroceliob3@gmail.com>
2022-07-04 22:07:11 -0300

    Projeto criado

---
+++ src/test/java/com/pedrocelio/crudemployees/CrudEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrocelio.crudemployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
